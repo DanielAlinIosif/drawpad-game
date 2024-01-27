8899db7c458ec4486c8df82b4a014437e369a9a0
aliniosidaniel <aliniosifdaniel@gmail.com>
2024-01-27 14:38:15 +0200

    git commit -m "

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const container = document.querySelector('#board');
-const SQUARE_NUMBER = 500;
+const squareNumber = 1000;
 
-for (let i = 0; i < SQUARE_NUMBER; i++) {
+for (let i = 0; i < squareNumber; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
