193fb2b656bd730b8af48d2bce3c5f418751177a
aliniosidaniel <aliniosifdaniel@gmail.com>
2024-02-01 19:30:43 +0200

    git committed -m

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const container = document.querySelector('#board');
-const squareNumber = 1000;
+const SQUARE_NUMBER = 500;
 
-for (let i = 0; i < squareNumber; i++) {
+for (let i = 0; i < SQUARE_NUMBER; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
+    square.addEventListener('mouseover', () => setColor(square))
+    ;
+    square.addEventListener('mouseleave', ( ) => removeColor(square))
+
     container.append(square);
 }
+
+function setColor(element) {
+    element.style.backgroundColor = getRandomColor();
+    element.style.boxshadow = getRandomColor();
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = "#1e1e1e";
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+
+    return colors[index];
+}
+
